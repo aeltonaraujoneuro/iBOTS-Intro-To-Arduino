280fb55ad71e715b615a4509d65d92fd7320d870
Aelton Araujo <aeltonaraujo@neuro.ufrn.br>
2024-02-21 00:56:05 +0100

    compiled and sending msgs

--- Day2/Session4/src/main.cpp
+++ Day2/Session4/src/main.cpp
@@ -1,7 +1,7 @@
 // Development Plan: Commit the code each time a step is completed.
-// [ ] Fill in the boilerplate Arduino code
-// [ ] Print the line "I'm ready!" to the serial port when the device starts.
-// [ ] Print the line "Still Ready..." every two seconds.
+// [x] Fill in the boilerplate Arduino code
+// [x Print the line "I'm ready!" to the serial port when the device starts.
+// [x] Print the line "Still Ready..." every two seconds.
 // [ ] Instead of "Still Ready", print the number of times that the loop() function has run.
 // [ ] Print the amount of time the device has been running, as well as which loop it is on, seperated by a comma.
 // [ ] Change the first text that is printed to be headers: "idx,time"
@@ -10,5 +10,15 @@
 // [ ] Calculate a running mean of the temperature over the last second, and add that as a column to the output.
 // [ ] Attach a blue LED and a red LED to the Arduino in the simulation.
 // [ ] Whenever the running mean is increasing, turn on the red LED.  Whenever it is decreasing, turn on the blue LED.
+#include <Arduino.h>
 
+void setup(){
+    Serial.begin(115200); // Qualquer taxa de transmissão deve funcionar
+    Serial.println("I'm ready!\n");
+}
 
+void loop(){
+    delay(2000);
+    Serial.println("Still Ready...\n");
+    
+}
