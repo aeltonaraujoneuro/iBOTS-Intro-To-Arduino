ab75b9804840eba7f6c65eb751397095ec042859
aeltonaraujoneuro <aeltonaraujo@neuro.ufrn.br>
2024-02-20 16:10:51 +0100

    session 1ok

--- Day2/Session1/src/main.cpp
+++ Day2/Session1/src/main.cpp
@@ -3,12 +3,12 @@
 // [x] This file is open in VSCode.
 // [x] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
 // [x] The Red LED is turned on.
-// [ ] The Green LED is also turned on.
-// [ ] The Blue LED is also turned on.
-// [ ] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
-// [ ] The Red LED then turns on and off every second, forever.
-// [ ] The Green LED simultaneously turns on and off every half-second, forever.
-// [ ] The Blue LED is on whenever the Green LED is off, and vice-versa.
+// [x] The Green LED is also turned on.
+// [x] The Blue LED is also turned on.
+// [x] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
+// [x] The Red LED then turns on and off every second, forever.
+// [x] The Green LED simultaneously turns on and off every half-second, forever.
+// [x] The Blue LED is on whenever the Green LED is off, and vice-versa.
 
 
 
