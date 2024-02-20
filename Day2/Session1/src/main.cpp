d79aa7db80efabb7c9ee88f981a69864f66e5299
aeltonaraujoneuro <aeltonaraujo@neuro.ufrn.br>
2024-02-20 15:12:30 +0100

    open simu

--- Day2/Session1/src/main.cpp
+++ Day2/Session1/src/main.cpp
@@ -1,8 +1,8 @@
 // Development Plan:  Check off each step with an 'x' as you complete each one:
 // [x] Showed up to the workshop.
-// [ ] This file is open in VSCode.
-// [ ] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
-// [ ] The Red LED is turned on.
+// [x] This file is open in VSCode.
+// [x] View the simulated Arduino by opening the diagram.json file (shows up when PlatformIO and WokWi Simulator extensions are installed).
+// [x] The Red LED is turned on.
 // [ ] The Green LED is also turned on.
 // [ ] The Blue LED is also turned on.
 // [ ] 300 milliseconds after all the LEDs turn on, the Blue LED turns off.
@@ -15,9 +15,29 @@
 #include <Arduino.h>
 
 void setup() {
+    // Turning on RED
+    pinMode(3,OUTPUT);
+    
+
+    // Turning GREEN on
+    pinMode(4,OUTPUT);
+    digitalWrite(4,HIGH);
+
+    // Turning BLUE on
+    pinMode(4,OUTPUT);
+    digitalWrite(4,HIGH);
 
 }
 
 void loop() {
-
+    digitalWrite(3,HIGH);
+    digitalWrite(4,HIGH);
+    delay(500);
+    digitalWrite(4,LOW);
+    delay(500);
+    digitalWrite(3,LOW);
+    digitalWrite(4,HIGH);
+    delay(500);
+    digitalWrite(4,LOW);
+    delay(500);
 }
